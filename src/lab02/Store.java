def39bd47b5a3ecdc16742ebb312da6842418c70
VietAnh <vietanha1k31@gmail.com>
2023-11-15 22:56:38 +0700

    bt7

---
+++ src/lab02/Store.java
@@ -0,0 +1,39 @@
+package lab02;
+
+public class Store {
+    public static final int MAX_NUMBERS = 999;
+    private final DVD[] itemsInStore = new DVD[MAX_NUMBERS];
+    int qtyOrdered;
+
+    public boolean checkFull() {
+        if (qtyOrdered < 20) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    public void addDVD(DVD disc) {
+        if (!checkFull()) {
+            itemsInStore[qtyOrdered] = disc;
+            System.out.println("Object added successfully.");
+            qtyOrdered++;
+        } else {
+            System.out.println("The store is full.");
+        }
+    }
+
+    public void addDVD(DVD[] discList) {
+        for(DVD disc: discList){
+            if (!checkFull()) {
+                itemsInStore[qtyOrdered] = disc;
+                System.out.println("Object added successfully.");
+                qtyOrdered++;
+            } else {
+                System.out.println("The store is full.");
+                return;
+            }
+        }
+    }
+}
